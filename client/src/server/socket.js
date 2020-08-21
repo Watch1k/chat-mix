0f5391bd7d89bd13894287da4fc43eaa42dab692
Oleksandr Filshyn <oleksandr.filshyn@globallogic.com>
2020-08-21 22:52:45 +0300

    cleanup

--- client/src/server/socket.js
+++ client/src/server/socket.js
@@ -4,4 +4,4 @@ export const ENDPOINT = process.env.NODE_ENV === 'production'
   ? 'https://chat-mix-server.herokuapp.com'
   : 'http://localhost:8000'
 
-export const socket = io(process.env.NODE_ENV)
+export const socket = io(ENDPOINT)
